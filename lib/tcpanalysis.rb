81e39556eadbd2fbb8bb30ff85a1b4d72814f8f5
Alex_Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-04-30 16:33:59 -0500

    Works well with sorting now

--- lib/tcpanalysis.rb
+++ lib/tcpanalysis.rb
@@ -16,15 +16,21 @@ class TCPAnalysis
 
     puts "How many packets do you want to capture?"
     @num_packets = gets.chomp
-
+    
     #run tcpdump
     tcpdump lanPort, @num_packets
+  
+  end
+
+  def self.analyzePcap pcap
+
+    `sudo mv #{pcap} tmp/pcap/`
 
     @xpl_dir = "tmp/xpl_#{Time.now.to_i.to_s}"
 
     `mkdir #{@xpl_dir}`
     #run tcptrace on the return pcap file
-    tcptrace @tcptraceOpts, @xpl_dir
+    tcptrace @tcptraceOpts, @xpl_dir, "tmp/pcap/#{pcap}"
 
     @gpl_dir = "tmp/gpl_#{Time.now.to_i.to_s}"
 
@@ -38,11 +44,12 @@ class TCPAnalysis
   end
 
   def self.tcpdump interface, num_packets
-    `sudo tcpdump -i #{interface} -c #{num_packets} -w tmp/pcap/out.pcap`
+    `sudo tcpdump -i #{interface} -c #{num_packets} -w out.pcap`
+    analyzePcap "out.pcap"
   end
   
-  def self.tcptrace options, dir 
-    `sudo tcptrace -#{options} --output_dir="#{dir}" tmp/pcap/out.pcap`
+  def self.tcptrace options, dir, pcap 
+    `sudo tcptrace -#{options} --output_dir="#{dir}" #{pcap}`
   end
 
   def self.getGpl xpl, gpl
@@ -53,36 +60,33 @@ class TCPAnalysis
   end
 
   def self.gnuplot gpl
-    @gpl_files = `find . -type f -print | grep .gpl`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
-    @gpl_files.each do |file|
-      `mv #{file} #{gpl}`
-    end
 
-    @dataset_files = `find . -type f -print | grep .datasets`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
-    @dataset_files.each do |file|
-      `mv #{file} #{gpl}`
-    end
+    move_files gpl, ".gpl"
+    move_files gpl, ".datasets"
+    move_files gpl, ".labels"
 
-    @label_files = `find . -type f -print | grep .labels`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
-    @label_files.each do |file|
-      `mv #{file} #{gpl}`
-    end
-
-    sort_into_dir gpl, "rtt"
-    sort_into_dir gpl, "tput"
-    sort_into_dir gpl, "ssize"
-    sort_into_dir gpl, "owin"
-    sort_into_dir gpl, "tsg"
-    sort_into_dir gpl, "tline"
+    sort_into_own_dir gpl, "rtt"
+    sort_into_own_dir gpl, "tput"
+    sort_into_own_dir gpl, "ssize"
+    sort_into_own_dir gpl, "owin"
+    sort_into_own_dir gpl, "tsg"
+    sort_into_own_dir gpl, "tline"
 
     #`gnuplot #{@gpl_files}`
   end
  
-  def self.sort_into_dir dir, pattern
+  def self.move_files gpl, pattern
+    @matched_files = `find . -type f -print | grep #{pattern}`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
+    @matched_files.each do |file|
+      `sudo mv #{file} #{gpl}`
+    end
+  end
+
+  def self.sort_into_own_dir dir, pattern
     @matched_files = `find #{dir} -type f -print | grep #{pattern}`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
     `mkdir #{dir}/#{pattern}`
     @matched_files.each do |file|
-      `mv #{file} #{dir}/#{pattern}`
+      `sudo mv #{file} #{dir}/#{pattern}`
     end
   end
 end
