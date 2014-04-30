2c8bbf97cb32f173b2779153cde7f8b91c700f81
Alex_Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-04-30 16:08:59 -0500

    So far with sorting into dirs

--- lib/tcpanalysis.rb
+++ lib/tcpanalysis.rb
@@ -1,4 +1,3 @@
-#require 'pcaprub
 
 class TCPAnalysis
 
@@ -21,13 +20,13 @@ class TCPAnalysis
     #run tcpdump
     tcpdump lanPort, @num_packets
 
-    @xpl_dir = "tmp/xpl_#{Time.now.to_i.to_s}/"
+    @xpl_dir = "tmp/xpl_#{Time.now.to_i.to_s}"
 
     `mkdir #{@xpl_dir}`
     #run tcptrace on the return pcap file
     tcptrace @tcptraceOpts, @xpl_dir
 
-    @gpl_dir = "tmp/gpl_#{Time.now.to_i.to_s}/"
+    @gpl_dir = "tmp/gpl_#{Time.now.to_i.to_s}"
 
     `mkdir #{@gpl_dir}`
 
@@ -35,10 +34,10 @@ class TCPAnalysis
     getGpl @xpl_dir, @gpl_dir
 
     #gnuplot
-    gnuplot
+    gnuplot @gpl_dir
   end
 
-  def self.tcpdump interface, num_packets 
+  def self.tcpdump interface, num_packets
     `sudo tcpdump -i #{interface} -c #{num_packets} -w tmp/pcap/out.pcap`
   end
   
@@ -49,13 +48,43 @@ class TCPAnalysis
   def self.getGpl xpl, gpl
     @xpl_files = `find #{xpl} -type f -print | grep .xpl`.split("\n").map{ |file| file.gsub('./', '')}
     @xpl_files.each do |file|
-      `sudo xpl2gpl #{file} > #{gpl}`
+      `sudo xpl2gpl #{file}`
     end 
   end
 
-  def self.gnuplot
-    @gpl_files = `find . -type f -print | grep .gpl`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ")
-    `gnuplot #{@gpl_files}`
+  def self.gnuplot gpl
+    @gpl_files = `find . -type f -print | grep .gpl`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
+    @gpl_files.each do |file|
+      `mv #{file} #{gpl}`
+    end
+
+    @dataset_files = `find . -type f -print | grep .datasets`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
+    @dataset_files.each do |file|
+      `mv #{file} #{gpl}`
+    end
+
+    @label_files = `find . -type f -print | grep .labels`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
+    @label_files.each do |file|
+      `mv #{file} #{gpl}`
+    end
+
+    sort_into_dir gpl, "rtt"
+    sort_into_dir gpl, "tput"
+    sort_into_dir gpl, "ssize"
+    sort_into_dir gpl, "owin"
+    sort_into_dir gpl, "tsg"
+    sort_into_dir gpl, "tline"
+
+    #`gnuplot #{@gpl_files}`
+  end
+ 
+  def self.sort_into_dir dir, pattern
+    @matched_files = `find #{dir} -type f -print | grep #{pattern}`.split("\n").map{ |file| file.gsub('./', '')}.sort().join(" ").split(" ")
+    `mkdir #{dir}/#{pattern}`
+    @matched_files.each do |file|
+      `mv #{file} #{dir}/#{pattern}`
+    end
   end
 end
 
+TCPAnalysis.run 'wlan0'
